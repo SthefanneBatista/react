98706e7667b994495acb3bcaf3c697f568d95088
SthefanneBatista <estudanteb73@gmail.com>
2020-01-31 22:57:43 -0200

    creating components

--- firststep/src/App.js
+++ firststep/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    nome : ''
+  }
+
+  modifyName = (event) => {
+    this.setState({
+      nome: event.target.value
+    })
+  }
+
+
+  render(){
+    return(
+    <>
+    <input type="text" value={this.state.nome} onChange={this.modifyName} />
+      <h1>Hello {this.state.nome}</h1>
+      </>
+    )
+  }
 }
 
 export default App;
